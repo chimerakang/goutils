74648a2a1853304bf7cca4655e6b19e2664842dc
chimerakang <chimerakang@gmail.com>
2022-01-14 15:00:45 +0800

    Add env.go, support check status when running docker image

---
+++ env/env.go
@@ -0,0 +1,17 @@
+package env
+
+import "os"
+
+func IsRunningInDockerContainer() bool {
+    // docker creates a .dockerenv file at the root
+    // of the directory tree inside the container.
+    // if this file exists then the viewer is running
+    // from inside a container so return true
+        
+    if _, err := os.Stat("/.dockerenv"); err == nil {
+        return true
+    }
+        
+    return false
+}
+
